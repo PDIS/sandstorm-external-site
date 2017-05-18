e2b0ff59692e843c25c7d65fbb2922cd968e0fb1
蔡仲明 (Romulus Urakagi Tsai) <urakagi@gmail.com>
2017-05-18 17:58:51 +0800

    Piping issue

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ var http = require("http"),
     path = require("path"),
     fs = require("fs"),
     doRequest = require('request'),
-    qs = require('querystring'),
     port = 8000;
-var bodyParser = require('body-parser');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var gToken;
@@ -17,162 +16,102 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-http.createServer(function (request, response) {
-  console.log(request.method + " " + request.url);
+app.get('/', sendLocalFile);
+app.get(/^\/lib/, sendLocalFile);
+app.get('/polis_ss_index.js', sendLocalFile);
+app.post('/claimToken', claimToken);
+app.get('/open_polis_conversation', function (request, response) {
+    var conversationId = request.query.conversation_id;
+    console.log(conversationId);
+    response.send('/' + conversationId);
+});
+app.all('*', pipe);
 
-  if (request.url === '/claimToken') {
-    claimToken(request, response);
-    return;
-  }
-  if (request.url === '/'
-	|| request.url === '/polis_ss_index.js'
-	|| request.url === '/lib/jquery-3.1.1.min.js') {
-    sendLocalFile(request, response);
-    return;
-  }
-  pipe(request.method, request.url, request, response);
-}).listen(port);
-
-console.log("Server running.");
+app.listen(port, function () {
+    console.log("Server running.");
+});
 
 function claimToken(request, response) {
-    var body = '';
+    console.log('Token claimed.');
     gUserId = request.headers["x-sandstorm-user-id"];
 
-    request.on('data', function (data) {
-        body += data;
-        // Too much POST data, kill the connection!
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-        if (body.length > 1e6)
-            request.connection.destroy();
-    });
-    request.on('end', function () {
-        var post = qs.parse(body);
-        var claimToken = post.requestToken;
-        var sessionId = request.headers["x-sandstorm-session-id"];
+    var post = request.body;
+    var claimToken = post.requestToken;
+    var sessionId = request.headers["x-sandstorm-session-id"];
 
-        doRequest({
-            proxy: process.env.HTTP_PROXY,
-            method: "POST",
-            url: "http://http-bridge/session/" + sessionId + "/claim",
-            json: {
-                requestToken: claimToken,
-                requiredPermissions: ["read"]
-            }
-        }, function (err, httpResponse, body) {
-            if (err) {
-                console.error(err);
-            } else {
-                saveAccessToken(body.cap, response);
-            }
-        });
+    doRequest({
+        proxy: process.env.HTTP_PROXY,
+        method: "POST",
+        url: "http://http-bridge/session/" + sessionId + "/claim",
+        json: {
+            requestToken: claimToken,
+            requiredPermissions: ["read"]
+        }
+    }, function (err, httpResponse, body) {
+        if (err) {
+            console.error(err);
+        } else {
+            saveAccessToken(body.cap, response);
+        }
     });
-
 }
 
 function saveAccessToken(token, response) {
     gToken = token;
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('OK');
-    response.end();
+    response.send('OK');
 }
 
-function saveAccessTokenDeprecated(token, response) {
-    gToken = token;
-    var body = [];
-    doRequest({
-            proxy: process.env.HTTP_PROXY,
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer " + token,
-            },
-	      url: "http://pol.is/2rieydnb3k"
-//            url: "http://pol.is/api/v3/conversations?polisApiKey=pkey_fhd7wkT3s9e8tw56J3H32dFa7s9"
-//	    uri: "http://pol.is/m/5kssnrx6mc?polisApiKey=pkey_fhd7wkT3s9e8tw56J3H32dFa7s9"
-            // url: "http://hostname/api/v3/conversations"
-//            url: "http://hostname/"
-        }
-    ).on('response', function (resp) {
-/*        for (var key in resp) {
-             if (typeof  resp[key] !== "function") {
-                 console.log(key + ": " + resp[key]);
-             }
-         }*/
-    }).on('error', function (err) {
-         for (var key in err) {
-             if (typeof  err[key] !== "function") {
-                 console.log(key + ": " + err[key]);
-             }
-         }
-        response.writeHead(200, {"Content-Type": "text/html"});
-        // response.writeHead(500, {"Content-Type": "text/html"});
-        response.write("Error:<br>" + err);
-        response.end();
-    }).on('data', function (chunk) {
-        body.push(chunk);
-    }).on('end', function () {
-        body = Buffer.concat(body).toString();
-        // console.log("Body: " + body);
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(body);
-        response.end();
-    });
-}
-
-function pipe(method, url, request, response) {
+function pipe(request, response) {
+    var method = request.method;
+    var url = request.url;
+    console.log('pipe ' + method + ' ' + url);
     var contentType = "text/html";
     if (method == "GET") {
-	    // Add user id
-	    if (url.indexOf('?') >= 0) {
-		url += "&xid=" + gUserId;
-	    } else {
-		url += "?xid=" + gUserId;
-	    }
+        // Add user id
+        if (url.indexOf('?') >= 0) {
+            url += "&xid=" + gUserId;
+        } else {
+            url += "?xid=" + gUserId;
+        }
     }
     var headers = {};
-//    var headers = request.headers;
     if (!headers) headers = {};
     headers.Authorization = "Bearer " + gToken;
     headers['content-type'] = request.headers['content-type'];
     headers['accept'] = request.headers['accept'];
     var config = {
-            proxy: process.env.HTTP_PROXY,
-            method: method,
-            headers: headers,
-            url: "http://hostname" + url
-         };
+        proxy: process.env.HTTP_PROXY,
+        method: method,
+        headers: headers,
+        url: "http://hostname" + url
+    };
     if (method == "POST") {
-	var body = '';
-        request.on('data', function (data) {
-            body += data;
-        });
-
-        request.on('end', function (){ 
-	    body = qs.parse(body);
-    	    if (body.ownerXid)
-		body.ownerXid = gUserId;
-    	    body.xid = gUserId;
-	    config.body = JSON.stringify(body);
-	    pipeRequest(config, response);
-       });
+        var body = request.body;
+        if (body.ownerXid) {
+            body.ownerXid = gUserId;
+        } else {
+            body.xid = gUserId;
+        }
+        config.body = JSON.stringify(body);
+        pipeRequest(config, response);
     } else {
-	pipeRequest(config, response);
+        pipeRequest(config, response);
     }
 }
 
 function pipeRequest(config, response) {
     var body = [];
     doRequest(config)
-      .on('response', function (resp) {
-        console.log("Responded " + resp.statusCode);
-        contentType = resp.headers['content-type'];
-        console.log(contentType);
-    }).on('error', function (err) {
-         for (var key in err) {
-             if (typeof  err[key] !== "function") {
-                 console.log(key + ": " + err[key]);
-             }
-         }
+        .on('response', function (resp) {
+            console.log("Responded " + resp.statusCode);
+            contentType = resp.headers['content-type'];
+            console.log(contentType);
+        }).on('error', function (err) {
+        for (var key in err) {
+            if (typeof  err[key] !== "function") {
+                console.log(key + ": " + err[key]);
+            }
+        }
         response.writeHead(200, {"Content-Type": contentType});
         // response.writeHead(500, {"Content-Type": "text/html"});
         response.write("Error:<br>" + err);
--- polis_ss_index.js
+++ polis_ss_index.js
@@ -1,89 +1,114 @@
 const KEY =
-'EAtQAQEAABEBF1EEAQH/x80lxnnjecgAQAMRCXoAAf9odHRwczovLwA/cG9sLmlz'
+    'EAtQAQEAABEBF1EEAQH/x80lxnnjecgAQAMRCXoAAf9odHRwczovLwA/cG9sLmlz'
 
 
-$().ready(function(){
+$().ready(function () {
 //	$('#open').on('click', open);
 });
 
 function open() {
 
-window.parent.postMessage({ powerboxRequest: { rpcId: 1, query: [ KEY 
-	], saveLabel: {defaultText: "your calendar, for adding events"},
-} }, "*");
+    window.parent.postMessage({
+        powerboxRequest: {
+            rpcId: 1, query: [
+                KEY,
+                "EAxQAQEAABEBF1EEAQH/x80lxnnjecgAQAMRCboAAf9odHRwczovLwFwcmVwcm9kLj9wb2wuaXM=" // prepod.polis.is
+            ], saveLabel: {defaultText: "your calendar, for adding events"},
+        }
+    }, "*");
 
-window.addEventListener("message", function (event) { if (event.source
-!== window.parent) {
-	// SECURITY: ignore postMessages that didn't come from the parent
-	// frame.
-	return; }
+    window.addEventListener("message", function (event) {
+        if (event.source
+            !== window.parent) {
+            // SECURITY: ignore postMessages that didn't come from the parent
+            // frame.
+            return;
+        }
 
-    var response = event.data;
+        var response = event.data;
 
-    if (response.rpcId !== 1) {
-	// Ignore RPC ID that dosen't match our request. (In real code
-	// you'd probably have a table of outstanding RPCs so that you
-	// don't have to register a separate handler for each one.)
-	return; }
+        if (response.rpcId !== 1) {
+            // Ignore RPC ID that dosen't match our request. (In real code
+            // you'd probably have a table of outstanding RPCs so that you
+            // don't have to register a separate handler for each one.)
+            return;
+        }
 
-    if (response.error) {
-	// Oops, something went wrong.
-	alert(response.error); return; }
+        if (response.error) {
+            // Oops, something went wrong.
+            alert(response.error);
+            return;
+        }
 
-    if (response.canceled) {
-	// The user closed the Powerbox without making a selection.
-	return; }
+        if (response.canceled) {
+            // The user closed the Powerbox without making a selection.
+            return;
+        }
 
-    // We now have a claim token. We need to send this to our server
-    // where we can exchange it for access to the remote API!
-    doClaimToken(response.token); }); }
+        // We now have a claim token. We need to send this to our server
+        // where we can exchange it for access to the remote API!
+        doClaimToken(response.token);
+    });
+}
 
-function doClaimToken(token) { 
-	$.ajax('/claimToken', { 
-		method: 'POST',
-		data: {"requestToken": token, "requiredPermissions": ["read"]}, 
-		success: function() {
-	createConversation();
-	return;
-	var mode = $('#mode').val();
-	var path = $('#path').val();
-	switch (mode) {
-		case "redirect":
-			location.href = path;
-			break;
-		case "get":
-			$('#response').load(path);
-			break;
-		case "post":
-			$.ajax(path, {
-				method: mode,
-				data: $('#data').val()	
-				}).done(function(resp){
-				$('#response').html(resp);
-			});
-			break;
-	}
-//			location.href = '/2rieydnb3k';
-//			location.href = '/other-conversations';
-//			$('body').load('/2rieydnb3k');
-	}
+function doClaimToken(token) {
+    $.ajax('/claimToken', {
+        method: 'POST',
+        data: {"requestToken": token, "requiredPermissions": ["read"]},
+        success: function () {
+            createConversation();
+            // var mode = $('#mode').val();
+            // var path = $('#path').val();
+            // switch (mode) {
+            //     case "redirect":
+            //         location.href = path;
+            //         break;
+            //     case "get":
+            //         $('#response').load(path);
+            //         break;
+            //     case "post":
+            //         $.ajax(path, {
+            //             method: mode,
+            //             data: $('#data').val()
+            //         }).done(function (resp) {
+            //             $('#response').html(resp);
+            //         });
+            //         break;
+            // }
+        }
     });
 }
 
 function createConversation() {
-	$.ajax('/api/v3/conversations', {
-		method: 'POST',
-		headers: {
-			"accept": "application/json",
-			"content-type": "application/json"
-		},
-		data: {
-			"polisApiKey":"pkey_mikeatpolis333",
-			"ownerXid": "handled by main.js"
-		}
-	}).done(function(resp){
-		$('body').html(resp);
-	});
+    $.post('/api/v3/conversations',
+        {
+            "polisApiKey": "pkey_fhd7wkT3s9e8tw56J3H32dFa7s9",
+            "ownerXid": "handled by main.js"
+        },
+        {
+            headers: {
+            "accept": "application/json",
+            "content-type": "application/json"
+            }
+        },
+        "application/json")
+        .done(function (resp) {
+        console.log('cp2: ' + resp);
+        var conversationId = JSON.parse(resp).conversation_id;
+        if (!conversationId) {
+            $('body').html(resp);
+            return;
+        }
+        openConversation(conversationId);
+        console.log('cp3');
+    });
+}
+
+function openConversation(conversationId) {
+    $.ajax('/open_polis_conversation?conversation_id=' + conversationId)
+        .done(function (resp) {
+            location.href = resp;
+        });
 }
 
 open();
